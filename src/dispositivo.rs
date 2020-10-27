15b173a7de75c1275de8b6d84094e1a8dacdd288
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 03:00:07 +0100

    Extiende dispositivo y plugin, avanza #1 y #2

--- src/dispositivo.rs
+++ src/dispositivo.rs
@@ -1,16 +1,37 @@
 use crate::error::Error;
 use crate::plugin::Plugin;
-struct Dispositivo {
+use std::net::IpAddr;
+
+pub(crate) struct Dispositivo {
     nombre: String,
+    activo: bool,
     solo_lectura: bool,
     plugin: Plugin,
+    ip: IpAddr,
 }
 
 impl Dispositivo {
-    fn obtener_estado() -> String {
+    fn buscar() -> Result<Vec<IpAddr>, Error<String>> {
         todo!()
     }
-    fn cambiar_estado() -> Result<(), Error<String>> {
+    fn nuevo(
+        nombre: &str,
+        solo_lectura: bool,
+        ip: IpAddr,
+        plugin: Plugin,
+    ) -> Result<Dispositivo, Error<String>> {
+        todo!()
+    }
+    fn encender(&self) -> Result<(), Error<String>> {
+        todo!()
+    }
+    fn apagar(&self) -> Result<(), Error<String>> {
+        todo!()
+    }
+    fn obtener_estado(&self) -> Result<String, Error<String>> {
+        todo!()
+    }
+    fn cambiar_estado(&self, estado: &str) -> Result<(), Error<String>> {
         todo!()
     }
 }
