1d774ba64c38639416b22bb63b98f6674018e553
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 20:07:42 +0100

    Mejorada macro error!(), closes #16

--- src/device_list.rs
+++ src/device_list.rs
@@ -1,24 +1,18 @@
-use crate::{device::Device, *};
 use crate::error::Error;
+use crate::{device::Device, *};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug,PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub(crate) struct DeviceList(Vec<Device>);
 
 impl DeviceList {
-    pub fn new(list : Vec<Device>) -> Self{
+    pub fn new(list: Vec<Device>) -> Self {
         Self(list)
     }
     pub fn to_json(&self) -> Result<String, Error> {
-        serde_json::to_string(&self).map_err(|e| {
-            let err_str = &e.to_string();
-            error!(err_str)
-        })
+        serde_json::to_string(&self).map_err(|e| error!(&e.to_string()))
     }
     pub fn from_json(json: &str) -> Result<DeviceList, Error> {
-        serde_json::from_str(json).map_err(|e| {
-            let err_str = &e.to_string();
-            error!(err_str)
-        })
+        serde_json::from_str(json).map_err(|e| error!(&e.to_string()))
     }
 }
