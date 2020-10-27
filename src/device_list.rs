769214f8bb083ca535ef05368d3a6fad87dc0409
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 14:50:40 +0100

    DeviceList implemeta ahora new() y PartialEq, arreglado tipo de retorno de from_json, avanza #14

--- src/device_list.rs
+++ src/device_list.rs
@@ -1,18 +1,21 @@
-use crate::device::Device;
+use crate::{device::Device, *};
 use crate::error::Error;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug,PartialEq)]
 pub(crate) struct DeviceList(Vec<Device>);
 
 impl DeviceList {
-    fn to_json(&self) -> Result<String, Error> {
+    pub fn new(list : Vec<Device>) -> Self{
+        Self(list)
+    }
+    pub fn to_json(&self) -> Result<String, Error> {
         serde_json::to_string(&self).map_err(|e| {
             let err_str = &e.to_string();
             error!(err_str)
         })
     }
-    fn from_json(json: &str) -> Result<String, Error> {
+    pub fn from_json(json: &str) -> Result<DeviceList, Error> {
         serde_json::from_str(json).map_err(|e| {
             let err_str = &e.to_string();
             error!(err_str)
