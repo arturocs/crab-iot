83395d5c74a831f44be4548bb2044b5a81475117
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 13:45:45 +0100

    Actualizada firma para el nuevo tipo Error y añadido soporte para json, avanza #9, #10, #11

--- src/dispositivo.rs
+++ src/dispositivo.rs
@@ -1,7 +1,8 @@
 use crate::error::Error;
 use crate::plugin::Plugin;
 use std::net::IpAddr;
-
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Dispositivo {
     nombre: String,
     activo: bool,
@@ -11,7 +12,7 @@ pub(crate) struct Dispositivo {
 }
 
 impl Dispositivo {
-    fn buscar() -> Result<Vec<IpAddr>, Error<String>> {
+    fn buscar() -> Result<Vec<IpAddr>, Error> {
         todo!()
     }
     fn nuevo(
@@ -19,19 +20,19 @@ impl Dispositivo {
         solo_lectura: bool,
         ip: IpAddr,
         plugin: Plugin,
-    ) -> Result<Dispositivo, Error<String>> {
+    ) -> Result<Dispositivo, Error> {
         todo!()
     }
-    fn encender(&self) -> Result<(), Error<String>> {
+    fn encender(&self) -> Result<(), Error> {
         todo!()
     }
-    fn apagar(&self) -> Result<(), Error<String>> {
+    fn apagar(&self) -> Result<(), Error> {
         todo!()
     }
-    fn obtener_estado(&self) -> Result<String, Error<String>> {
+    fn obtener_estado(&self) -> Result<String, Error> {
         todo!()
     }
-    fn cambiar_estado(&self, estado: &str) -> Result<(), Error<String>> {
+    fn cambiar_estado(&self, estado: &str) -> Result<(), Error> {
         todo!()
     }
 }
