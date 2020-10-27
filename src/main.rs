31a7c781dcb7811cdea1c4b2480c1e7c56b68ee3
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 13:49:12 +0100

    Añadidos atributos para macros

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-#![allow(dead_code, unused_variables)]
+#![allow(dead_code, unused_variables, unused_macros)]
 mod dispositivo;
 mod plugin;
+#[macro_use]
 mod error;
+mod lista_dispositivos;
+
 fn main() {
     todo!()
 }
