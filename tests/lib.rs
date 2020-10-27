aed8445b2d4206d962589e77d6300449d7a9c496
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 14:53:01 +0100

    Añadido test de deserialización de lista de dispositivos, avanza #14

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,30 @@
+#![allow(dead_code, unused_variables, unused_macros)]
+#[path = "../src/device.rs"]
+mod device;
+#[path = "../src/device_list.rs"]
+mod device_list;
+#[path = "../src/error.rs"]
+mod error;
+#[path = "../src/plugin.rs"]
+mod plugin;
+use std::net::{IpAddr, Ipv4Addr};
+
+use device::Device;
+use device_list::DeviceList;
+
 #[test]
 fn test_vacio() {
     assert_eq!((), ());
 }
-
+#[test]
+fn deserialize_device_list() {
+    let devices_from_json = DeviceList::from_json(
+        r#"[{"name":"prueba","active":false,"read_only":false,"plugin":null,"ip":"127.0.0.1"}]"#,
+    )
+    .unwrap();
+    let devices = DeviceList::new(vec![Device::without_plugin("prueba",false,
+        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+    )]);
+    dbg!(&devices);
+    assert_eq!(devices, devices_from_json);
+}
