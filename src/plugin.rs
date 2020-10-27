e1c1fcddc012dacff05909bebf8d40d9043b7d96
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-27 13:45:45 +0100

    Actualizada firma para el nuevo tipo Error y añadido soporte para json

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 use std::path::PathBuf;
-
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Plugin {
     nombre_dispositivo: String,
     id_dispositivo: String,
@@ -11,10 +12,10 @@ impl Plugin {
     fn cargar(nombre: &str, ruta: &str) -> Plugin {
         todo!()
     }
-    fn ejecutar(&self) -> Result<(), Error<String>> {
+    fn ejecutar(&self) -> Result<(), Error> {
         todo!()
     }
-    fn parar_ejecucion(&self) -> Result<(), Error<String>> {
+    fn parar_ejecucion(&self) -> Result<(), Error> {
         todo!()
     }
 }
