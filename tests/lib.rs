f3a406aba54d1f0402b2c04a78e7eb9f8b005c67
Arturo Cortés Sánchez <arturocs@protonmail.com>
2020-10-28 19:44:00 +0100

    Adaptado test al nuevo tipo Device, avanza #18

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code, unused_variables, unused_macros)]
-#[path = "../src/device.rs"]
+#[path = "../src/device/mod.rs"]
 mod device;
 #[path = "../src/device_list.rs"]
 mod device_list;
@@ -10,6 +10,7 @@ mod plugin;
 use std::net::{IpAddr, Ipv4Addr};
 
 use device::Device;
+use device::rdevice::RDevice;
 use device_list::DeviceList;
 
 #[test]
@@ -19,12 +20,13 @@ fn test_vacio() {
 #[test]
 fn deserialize_device_list() {
     let devices_from_json = DeviceList::from_json(
-        r#"[{"name":"prueba","active":false,"read_only":false,"plugin":null,"ip":"127.0.0.1"}]"#,
+        r#"[{"ReadOnly":{"name":"prueba","active":false,"plugin":null,"ip":"127.0.0.1"}}]"#,
     )
     .unwrap();
-    let devices = DeviceList::new(vec![Device::without_plugin("prueba",false,
+    let devices = DeviceList::new(vec![Device::ReadOnly(RDevice::without_plugin(
+        "prueba",
         IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-    )]);
+    ))]);
     dbg!(&devices);
     assert_eq!(devices, devices_from_json);
 }
