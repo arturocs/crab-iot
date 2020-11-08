caf54386b88ed140ec48607258e2df0bb3fa6978
Arturo <arturocs@protonmail.com>
2020-11-08 23:56:38 +0100

    Creados traits Readable y Writable para los modulos device, avanza #30

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,17 +1,25 @@
-pub mod device_attributes;
 pub mod rdevice;
 pub mod rwdevice;
-use self::{rdevice::RDevice, rwdevice::RWDevice};
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::error::Error;
 use std::net::IpAddr;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub(crate) enum Device {
-    ReadOnly(RDevice),
-    ReadWrite(RWDevice),
+pub fn local_search() -> Result<Vec<IpAddr>, Box<dyn Error>> {
+    todo!()
 }
-impl Device {
-    pub fn local_search() -> Result<Vec<IpAddr>, crate::error::Error> {
-        todo!()
-    }
+pub(crate) trait Readable {
+    fn new(
+        name: &str,
+        plugin_name: &str,
+        plugin_path: &str,
+        ip: &str,
+    ) -> Result<Self, Box<dyn Error>>
+    where
+        Self: std::marker::Sized;
+    fn get_ip(&self) -> IpAddr;
+    fn get_name(&self) -> String;
+    fn get_status(&self) -> Result<Value, Box<dyn Error>>;
+}
+pub(crate) trait Writable: Readable {
+    fn set_status(&self, status: &Value) -> Result<Value, Box<dyn Error>>;
 }
