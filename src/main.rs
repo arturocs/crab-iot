b2c501a7b6a9d0645da3a79edde35a2e6c550415
Arturo <arturocs@protonmail.com>
2020-11-08 23:58:48 +0100

    Implementada una prueba de api rest, avanza #1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 #![allow(dead_code, unused_variables, unused_macros)]
+
 mod device;
 mod device_list;
 mod error;
 mod plugin;
+use device::{rwdevice::RWDevice, Readable};
+use warp::Filter;
 
-fn main() {
-    todo!()
+#[tokio::main]
+async fn main() {
+    let api = warp::path!("api").map(|| {
+        let mockup_device = RWDevice::new(
+            "mockup_device",
+            "fake_plugin",
+            "./target/debug/libfake_plugin.so",
+            "127.0.0.1",
+        )
+        .unwrap();
+        let status = mockup_device.get_status().unwrap();
+        status.get("data").unwrap().to_string()
+    });
+
+    warp::serve(api).run(([127, 0, 0, 1], 3030)).await;
 }
