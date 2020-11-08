9ba7d1c250f999b7c512c0335c31a0bd637ec8d7
Arturo <arturocs@protonmail.com>
2020-11-08 23:52:12 +0100

    Rediseñado modulo de plugins, avanza #6

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,21 +1,39 @@
-use crate::error::Error;
-use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+use libloading::{Library, Symbol};
+use serde_json::Value;
+use std::error::Error;
+use std::{ffi::CString, os::raw::c_char};
+#[derive(Debug)]
 pub(crate) struct Plugin {
     device_name: String,
-    device_id: String,
-    plugin_path: PathBuf,
+    dylib: Library,
 }
 
 impl Plugin {
-    fn load(nombre: &str, ruta: &str) -> Plugin {
-        todo!()
+    pub(crate) fn get_status(&self) -> Result<Value, Box<dyn Error>> {
+        let cstr = unsafe {
+            let func: Symbol<unsafe extern "C" fn() -> *mut c_char> =
+                self.dylib.get(b"get_status")?;
+            CString::from_raw(func())
+        };
+        let s = cstr.to_str()?;
+        let json: Value = serde_json::from_str(s)?;
+        Ok(json)
     }
-    fn execute(&self) -> Result<(), Error> {
-        todo!()
+    pub(crate) fn set_status(&self, status: &Value) -> Result<Value, Box<dyn Error>> {
+        let status_str = CString::new(status.to_string()).unwrap().into_raw();
+        let cstr = unsafe {
+            let func: Symbol<unsafe extern "C" fn(*const c_char) -> *mut c_char> =
+                self.dylib.get(b"set_status")?;
+            CString::from_raw(func(status_str))
+        };
+        let s = cstr.to_str()?;
+        let json: Value = serde_json::from_str(s)?;
+        Ok(json)
     }
-    fn stop_execution(&self) -> Result<(), Error> {
-        todo!()
+    pub(crate) fn load(name: &str, path: &str) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            device_name: name.to_string(),
+            dylib: Library::new(path)?,
+        })
     }
 }
