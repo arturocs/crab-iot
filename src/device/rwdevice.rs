aec082fb60d21b52bea1c8441c8d65252fb956eb
Arturo <arturocs@protonmail.com>
2020-11-09 01:57:44 +0100

    Cambia el tipo de dato error en rwdevice, avanza #32 y closes #26

--- src/device/rwdevice.rs
+++ src/device/rwdevice.rs
@@ -1,8 +1,7 @@
 use crate::plugin::Plugin;
 use serde_json::Value;
-
+use crate::{error::Error, *};
 use super::{Readable, Writable};
-use std::error::Error;
 use std::net::IpAddr;
 #[derive(Debug)]
 pub(crate) struct RWDevice {
@@ -17,27 +16,27 @@ impl Readable for RWDevice {
         plugin_name: &str,
         plugin_path: &str,
         ip: &str,
-    ) -> Result<Self, Box<dyn Error>> {
+    ) -> Result<Self,  Error> {
         Ok(Self {
             name: name.to_string(),
-            plugin: Plugin::load(plugin_name, plugin_path)?,
-            ip: "127.0.0.1".parse()?,
+            plugin: Plugin::load(plugin_name, plugin_path).map_err(|e| error!(e))?,
+            ip: "127.0.0.1".parse().map_err(|e| error!(e))?,
         })
     }
-    fn get_status(&self) -> Result<Value, Box<dyn Error>> {
+    fn get_status(&self) -> Result<Value, Error> {
         self.plugin.get_status()
     }
 
     fn get_ip(&self) -> IpAddr {
-        todo!()
+        *&self.ip
     }
 
     fn get_name(&self) -> String {
-        todo!()
+        (&self).name.clone()
     }
 }
 impl Writable for RWDevice {
-    fn set_status(&self, status: &Value) -> Result<Value, Box<dyn std::error::Error>> {
+    fn set_status(&self, status: &Value) -> Result<Value, Error> {
         self.plugin.set_status(status)
     }
 }
