315b4df64117d115bcf2d26f879bfdfb3a0e16a8
Arturo <arturocs@protonmail.com>
2020-11-09 01:56:08 +0100

    Cambiado el tipo Error en la declaración de los traits, avanza #32 y #26

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,25 +1,19 @@
 pub mod rdevice;
 pub mod rwdevice;
+use crate::error::Error;
 use serde_json::Value;
-use std::error::Error;
 use std::net::IpAddr;
-
-pub fn local_search() -> Result<Vec<IpAddr>, Box<dyn Error>> {
+pub(crate) fn local_search() -> Result<Vec<IpAddr>, Error> {
     todo!()
 }
 pub(crate) trait Readable {
-    fn new(
-        name: &str,
-        plugin_name: &str,
-        plugin_path: &str,
-        ip: &str,
-    ) -> Result<Self, Box<dyn Error>>
+    fn new(name: &str, plugin_name: &str, plugin_path: &str, ip: &str) -> Result<Self, Error>
     where
         Self: std::marker::Sized;
     fn get_ip(&self) -> IpAddr;
     fn get_name(&self) -> String;
-    fn get_status(&self) -> Result<Value, Box<dyn Error>>;
+    fn get_status(&self) -> Result<Value, Error>;
 }
 pub(crate) trait Writable: Readable {
-    fn set_status(&self, status: &Value) -> Result<Value, Box<dyn Error>>;
+    fn set_status(&self, status: &Value) -> Result<Value, Error>;
 }
