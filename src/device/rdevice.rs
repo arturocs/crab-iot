4e5ac69220fe6824b71b79dce47827011713f1be
Arturo <arturocs@protonmail.com>
2020-11-09 01:56:48 +0100

    Cambia el tipo de error en rdevice, avanza #32 y 26

--- src/device/rdevice.rs
+++ src/device/rdevice.rs
@@ -1,8 +1,7 @@
 use crate::plugin::Plugin;
 use serde_json::Value;
-use std::error::Error;
 use std::net::IpAddr;
-
+use crate::{error::Error, *};
 use super::Readable;
 
 #[derive(Debug)]
@@ -18,14 +17,14 @@ impl Readable for RDevice {
         plugin_name: &str,
         plugin_path: &str,
         ip: &str,
-    ) -> Result<Self, Box<dyn Error>> {
+    ) -> Result<Self, Error> {
         Ok(Self {
             name: name.to_string(),
-            plugin: Plugin::load(plugin_name, plugin_path)?,
-            ip: "127.0.0.1".parse()?,
+            plugin: Plugin::load(plugin_name, plugin_path).map_err(|e| error!(e))?,
+            ip: "127.0.0.1".parse().map_err(|e| error!(e))?,
         })
     }
-    fn get_status(&self) -> Result<Value, Box<dyn Error>> {
+    fn get_status(&self) -> Result<Value,  Error> {
         self.plugin.get_status()
     }
 
