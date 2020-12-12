2df7eae9a6593358a97fd88925a42422bfcecc7b
Arturo <arturocs@protonmail.com>
2020-12-12 02:03:05 +0100

    Adaptado modulo plugin al nuevo intercambio de información, avanza #43

--- device/src/plugin.rs
+++ device/src/plugin.rs
@@ -1,8 +1,8 @@
-use crate::{error::Error, *};
 use libloading::{Library, Symbol};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::{ffi::CString, os::raw::c_char, path::PathBuf};
+use std::path::PathBuf;
+use utils::{error::Error, *};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Plugin {
     device_name: String,
@@ -19,31 +19,26 @@ impl PartialEq for Plugin {
 
 impl Plugin {
     pub(crate) fn get_status(&self, query: &Value) -> Result<Value, Error> {
-        let query_str = CString::new(query.to_string()).unwrap().into_raw();
         if let Some(lib) = &self.dylib {
-            let cstr = unsafe {
-                let func: Symbol<unsafe extern "C" fn(*const c_char) -> *mut c_char> =
+            let status = unsafe {
+                let func: Symbol<unsafe extern "C" fn(*const Value) -> *mut Result<Value, Error>> =
                     lib.get(b"get_status").map_err(|e| error!(e))?;
-                CString::from_raw(func(query_str))
+                Box::from_raw(func(query)).map_err(|e| error!(e))?
             };
-            let s = cstr.to_str().map_err(|e| error!(e))?;
-            let json: Value = serde_json::from_str(s).map_err(|e| error!(e))?;
-            Ok(json)
+            Ok(status)
         } else {
-            Err(error!("Unloaded plugin, reload_after_deserialize()"))
+            Err(error!("Unloaded plugin, call reload_after_deserialize()"))
         }
     }
+
     pub(crate) fn set_status(&self, status: &Value) -> Result<Value, Error> {
-        let status_str = CString::new(status.to_string()).unwrap().into_raw();
         if let Some(lib) = &self.dylib {
-            let cstr = unsafe {
-                let func: Symbol<unsafe extern "C" fn(*const c_char) -> *mut c_char> =
+            let plugin_answer = unsafe {
+                let func: Symbol<unsafe extern "C" fn(*const Value) -> *mut Result<Value, Error>> =
                     lib.get(b"set_status").map_err(|e| error!(e))?;
-                CString::from_raw(func(status_str))
+                Box::from_raw(func(status)).map_err(|e| error!(e))?
             };
-            let s = cstr.to_str().map_err(|e| error!(e))?;
-            let json: Value = serde_json::from_str(s).map_err(|e| error!(e))?;
-            Ok(json)
+            Ok(plugin_answer)
         } else {
             Err(error!("Unloaded plugin, call reload_after_deserialize()"))
         }
