6fb1a4b51eed0e1ad630415f9096af3bc21319d1
Arturo <arturocs@protonmail.com>
2020-12-12 02:30:08 +0100

    Actualizado weather_fake_plugin a la nueva API, avanza #43

--- weather_fake_plugin/src/lib.rs
+++ weather_fake_plugin/src/lib.rs
@@ -1,55 +1,55 @@
 use once_cell::sync::Lazy;
 use serde::Deserialize;
-use std::{ffi::CString, os::raw::c_char};
+use serde_json::Value;
 use utils::weather_json_schema;
+use utils::{error::Error, *};
 
 static WEATHER: Lazy<weather_json_schema::Root> = Lazy::new(|| {
     let json = include_str!("../1769_es.json");
     serde_json::from_str(json).unwrap()
 });
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 enum Query {
     Forecast { day: u8 },
     Climate { month: u8 },
 }
 
+fn process_query(query: &Value) -> Result<Value, Error> {
+    let query = query
+        .get("query")
+        .ok_or(error!("Query missing 'query' field"))?;
+    let query = serde_json::from_value(query.clone()).map_err(|e| error!(e))?;
+    match query {
+        Query::Forecast { day } => {
+            if day <= 3 {
+                let forecast_day = &WEATHER.city.forecast.forecast_day[day as usize];
+                serde_json::to_value(forecast_day).map_err(|e| error!(e))
+            } else {
+                Err(error!("Maximum number of days allowed is 3"))
+            }
+        }
+        Query::Climate { month } => {
+            if month <= 11 {
+                let climate_month = &WEATHER.city.climate.climate_month[month as usize];
+                serde_json::to_value(climate_month).map_err(|e| error!(e))
+            } else {
+                Err(error!("Maximum number of months allowed is 11"))
+            }
+        }
+    }
+}
+
 /// # Safety
 ///
-/// query_str should come from from calling into_raw() on a CString.
+/// query should come from from calling Box::into_raw() in a boxed serde_json::Value.
 
 #[no_mangle]
-pub unsafe extern "C" fn get_status(query_str: *mut c_char) -> *mut c_char {
-    let query_cstr = CString::from_raw(query_str);
-    let query: Result<Query, serde_json::Error> =
-        serde_json::from_str(query_cstr.to_str().unwrap());
-    match query {
-        Ok(q) => match q {
-            Query::Forecast { day } => {
-                if day <= 3 {
-                    let forecast_day = &WEATHER.city.forecast.forecast_day[day as usize];
-                    let forecast_string = serde_json::to_string(forecast_day).unwrap();
-                    CString::new(forecast_string).unwrap().into_raw()
-                } else {
-                    CString::new(r#"{"error": "Maximum number of days allowed is 3"}"#)
-                        .unwrap()
-                        .into_raw()
-                }
-            }
-            Query::Climate { month } => {
-                if month <= 11 {
-                    let climate_month = &WEATHER.city.climate.climate_month[month as usize];
-                    let climate_string = serde_json::to_string(climate_month).unwrap();
-                    CString::new(climate_string).unwrap().into_raw()
-                } else {
-                    CString::new(r#"{"error": "Maximum number of months allowed is 11"}"#)
-                        .unwrap()
-                        .into_raw()
-                }
-            }
-        },
-        Err(e) => CString::new(format!(r#"{{"error": "{}"}}"#, e))
-            .unwrap()
-            .into_raw(),
-    }
+pub unsafe extern "C" fn get_status(query: *const Value) -> *mut Result<Value, Error> {
+    let query = query.as_ref();
+    let status = match query {
+        Some(v) => process_query(v),
+        None => Err(error!("Unable to dereference query")),
+    };
+    Box::into_raw(Box::new(status))
 }
